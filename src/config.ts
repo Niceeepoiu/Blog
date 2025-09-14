be43717f772c151b10565ce1b41e00308d170dd5
Niceeepoiu <abefgpoiu@gmail.com>
2025-09-14 09:24:02 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Niceeepoiu",
-	subtitle: "博客",
+	subtitle: "时之裂缝",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -50,7 +50,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/user.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Niceeepoiu",
-	bio: "Ride the wind and waves!",
+	bio: "斩神之旅，无惧天道！",
 	links: [
 		{
 			name: "GitHub",
